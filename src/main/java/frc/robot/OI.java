8c0f62783778e1830fc87fb9d0092ea71389b790
Whee-il <willcockrum5@gmail.com>
2019-02-16 10:09:32 -0800

    I changed the buttons for the lift auto commands

--- src/main/java/frc/robot/OI.java
+++ src/main/java/frc/robot/OI.java
@@ -79,8 +79,8 @@ public class OI implements RobotMap{
       //LiftDown = new JoystickButton(liftStick, 7);
       ResetLift = new JoystickButton(liftStick, 2);//temporary button
       LiftLevel1 = new JoystickButton(liftStick, 6);//temporary button
-      LiftLevel2 = new JoystickButton(liftStick, 4);//temporary button
-      LiftLevel3 = new JoystickButton(liftStick, 5);//temporary button
+      LiftLevel2 = new JoystickButton(liftStick, 10);//temporary button
+      LiftLevel3 = new JoystickButton(liftStick, 11);//temporary button
       
       PistonOut = new JoystickButton(mainStick, 8);
       PistonIn = new JoystickButton(mainStick, 7);
